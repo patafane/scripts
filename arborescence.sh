7e44d31e3fc0d18dcb382a495b77fee77811cb5b
PATAFANE <patafane@MacBook-Air-de-PATAFANE.local>
2023-11-27 15:44:32 +0100

    maj arbo

--- arborescence.sh
+++ arborescence.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 touch index.html
-mkdir -p public/{css,pages,img,js}
-touch public/css/style.css && touch public/js/main.js && touch public/pages/about.html
+mkdir -p public/{css,pages,img,js,sass}
+touch public/css/style.css && touch public/js/main.js && touch public/pages/about.html && touch public/sass/style.sass
