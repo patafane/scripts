d98251b51197203807cdc41bd2dee02445a3c6c2
patafane <cecilienhelfter@gmail.com>
2023-11-28 09:10:39 +0100

    sass mis a jour

--- arborescence.sh
+++ arborescence.sh
@@ -2,4 +2,4 @@
 touch index.html
 mkdir -p public/{css,pages,img,js,sass/modules}
 touch public/css/style.css && touch public/js/main.js && touch public/pages/about.html && touch public/sass/app.sass
-touch public/sass/modules/_style.sass && touch public/sass/modules/_colors.sass
+touch public/sass/modules/_style.sass && touch public/sass/_variables.sass
