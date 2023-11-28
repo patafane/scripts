ac2d503bc353500aff321337c8598d74fd04478a
patafane <cecilienhelfter@gmail.com>
2023-11-28 09:04:03 +0100

    sass maj 2.0

--- arborescence.sh
+++ arborescence.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 touch index.html
-mkdir -p public/{css,pages,img,js,sass/{modules}}
+mkdir -p public/{css,pages,img,js,sass/modules}
 touch public/css/style.css && touch public/js/main.js && touch public/pages/about.html && touch public/sass/app.sass
 touch public/sass/modules/_style.sass && touch public/sass/modules/_colors.sass
