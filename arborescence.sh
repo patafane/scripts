7eb24fc169f0d703be55cb75af1e83fe116bf4d6
PATAFANE <patafane@MacBook-Air-de-PATAFANE.local>
2023-12-04 13:42:36 +0100

    rectifications

--- arborescence.sh
+++ arborescence.sh
@@ -3,7 +3,7 @@ touch index.html
 mkdir -p public/{css,pages,img,js,src/sass/modules}
 touch public/css/style.css && touch public/js/main.js && touch public/pages/about.html && touch public/src/sass/app.sass
 touch public/src/sass/modules/_style.sass && touch public/src/sass/_variables.sass && touch public/src/sass/modules/_header.sass && touch public/src/sass/modules/_nav.sass && touch public/src/sass/modules/_section.sass && touch public/src/sass/modules/_footer.sass
-echo "<!DOCTYPE html>
+echo '<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -16,7 +16,7 @@ echo "<!DOCTYPE html>
     
     <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
 </body>
-</html>" > index.html
+</html>' > index.html
 echo "@import ./_variables
 @import ../../../node_modules/bootstrap/scss/bootstrap
 @import ./modules/_style" > public/src/sass/app.sass
