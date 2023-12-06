667a00124e67412b2bbd0ace0a0c215bc08faa56
patafane <cecilienhelfter@gmail.com>
2023-12-06 14:12:53 +0100

    mise a jour fontawesome

--- arborescence.sh
+++ arborescence.sh
@@ -10,8 +10,8 @@ echo '<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.css">
     <link rel="stylesheet" href="public/css/style.css">
-    <script src="https://kit.fontawesome.com/08d55bf215.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
